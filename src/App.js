d6c7451ef16fb8369614f5689de3ae44359f9a69
gabeaster <gab.easter@live.com>
2020-04-16 15:57:21 -0400

    in App.js, const addItem is now functional. button can be clicked to add to cart, each item added to cart increases the number counter in the nav bar. when cart is clicked on, the items are displayed.

--- src/App.js
+++ src/App.js
@@ -7,12 +7,20 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+
+//We have a function called app that returns a Div (.App) that renders a Navigation component, a Product Component routed component, and a ShoppingCart routed component
+//We need to complete the addItem functionality
+
 function App() {
+	//two pieces of state: products, and cart
+	//products receives data as initialState
 	const [products] = useState(data);
+	//cart receives an empty array as initialStae
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item]);
 	};
 
 	return (
@@ -21,10 +29,12 @@ function App() {
 
 			{/* Routes */}
 			<Route exact path="/">
+				{/* Products is receiving products and addItem as props */}
 				<Products products={products} addItem={addItem} />
 			</Route>
 
 			<Route path="/cart">
+				{/* ShoppingCart is receiving cart as props */}
 				<ShoppingCart cart={cart} />
 			</Route>
 		</div>
